83c82b804222f44bd5ce0b338e2de7eff1dd1dd6
Said guyna <guinasaid@gmail.com>
2021-04-13 10:19:54 +0200

    Tarea1

--- src/pk_SuperBuscaMinas/Tablero.java
+++ src/pk_SuperBuscaMinas/Tablero.java
@@ -442,6 +442,14 @@ public class Tablero {
 	 * Método que imprime una matriz por pantalla
 	 */
 	public static void imprimeMatrizBotones(BotonMina[][] matrizBotones) {
-// TODO
+		for (int i = 0; i < matrizBotones.length; i++) {
+			System.out.print("\n(");
+			for (int j = 0; j < matrizBotones[0].length; j++) {
+				System.out.print( "  " + matrizBotones[i][j] + "  ");
+			}
+			System.out.print(")\n\n");
+		}
+	    
+
 	}		
 }
